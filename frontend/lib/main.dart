e567ac841f3c2e94c1c3ce055d7ac2272681042b
Aryan Nagori <aryannikill123@gmail.com>
2026-04-04 11:26:27 +0530

    update main.dart to use SplashScreen as home

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
+import 'config/theme.dart';
 import 'services/api_service.dart';
 import 'providers/auth_provider.dart';
-import 'screens/auth/login_screen.dart';
-import 'screens/home/home_screen.dart';
+import 'screens/splash_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   try {
-    await dotenv.load(fileName: ".env");
+    await dotenv.load(fileName: '.env');
   } catch (e) {
     debugPrint('Warning: .env file not found');
   }
-  
+
   runApp(const MyApp());
 }
 
@@ -32,43 +32,10 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         title: 'Classly',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          useMaterial3: true,
-        ),
-        home: const AuthWrapper(),
+        theme: AppTheme.lightTheme,
+        home: const SplashScreen(),
         debugShowCheckedModeBanner: false,
       ),
-    );
-  }
-}
-
-class AuthWrapper extends StatefulWidget {
-  const AuthWrapper({Key? key}) : super(key: key);
-
-  @override
-  State<AuthWrapper> createState() => _AuthWrapperState();
-}
-
-class _AuthWrapperState extends State<AuthWrapper> {
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<AuthProvider>().checkAuthStatus();
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer<AuthProvider>(
-      builder: (context, authProvider, child) {
-        if (authProvider.isAuthenticated) {
-          return const HomeScreen();
-        } else {
-          return const LoginScreen();
-        }
-      },
     );
   }
 }
